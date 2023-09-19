f86b5a28eb4c6a2562436af77a3999f427eac58b
Richard Bendler <richard.bendler@gmail.com>
2023-09-19 23:45:15 +0200

    dependency navigator

--- OLD_Trinkspiel_RN/VariablesContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const VariablesContext = React.createContext({
-  playerNames: [],
-  setPlayerNames: () => {},
-  drinkTypes: [], // Initialisieren mit leerem Array
-  setDrinkTypes: () => {},
-});
-
