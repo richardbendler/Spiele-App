e49b1bc48c04ad34cf67f76f59cb72405e13ce79
Flowwrian <flo.haberkorn@gmx.de>
2023-10-12 17:18:37 +0200

    players can now be removed

--- Trinkspiel_RN/src/menus/sublements/AddPlayerNameContainer.js
+++ Trinkspiel_RN/src/menus/sublements/AddPlayerNameContainer.js
@@ -5,35 +5,60 @@ import { VariablesContext } from '../../../VariablesContext';
 
 const NameContainer = ({ player }) => {
 
-        const { settingsVisible, setSettingsVisible } = useContext(VariablesContext);
-    
-        const { playerNames, setPlayerNames } = useContext(VariablesContext);
-        // Ändern Sie die Variable "drinks" für einen bestimmten Spieler
-        const togglePlayerDrinks = (playerIndex) => {
-            setPlayerNames((prevPlayerNames) => {
-                const updatedPlayerNames = [...prevPlayerNames]; // Erstellen Sie eine Kopie der Spielerliste
-                updatedPlayerNames[playerIndex].drinks = !updatedPlayerNames[playerIndex].drinks; // Ändern Sie den Wert
-                return updatedPlayerNames; // Geben Sie die aktualisierte Liste zurück
-            });
-        };
-    
-        const [playerDrinks, setPlayerDrinks] = useState(player.drinks);
-    
-    
-    
-        return (
-            <View style={appStyles.playerListText}>
-                <Text style={{ color: 'white', paddingRight: '10%' }}>{player.name}</Text>
-                <Button 
-                    onPress= {() => {
+    const { settingsVisible, setSettingsVisible } = useContext(VariablesContext);
+
+    const { playerNames, setPlayerNames } = useContext(VariablesContext);
+    // Ändern Sie die Variable "drinks" für einen bestimmten Spieler
+    const togglePlayerDrinks = (playerObject) => {
+        setPlayerNames((prevPlayerNames) => {
+            const updatedPlayerNames = [...prevPlayerNames]; // Erstellen Sie eine Kopie der Spielerliste
+            const index = updatedPlayerNames.indexOf(player);
+            updatedPlayerNames[index].drinks = !updatedPlayerNames[index].drinks; // Ändern Sie den Wert
+            return updatedPlayerNames; // Geben Sie die aktualisierte Liste zurück
+        });
+    };
+
+    const removePlayer = (playerObject) => {
+        index = playerNames.indexOf(playerObject);
+        if (index > -1) {
+            let newPlayerList = [...playerNames];
+            newPlayerList.splice(index, 1);
+            setPlayerNames(newPlayerList);
+            console.log("removed player");
+        } else {
+            console.log("Could not find player");
+        }
+    }
+
+
+    const [playerDrinks, setPlayerDrinks] = useState(player.drinks);
+
+
+
+    return (
+        <View style={appStyles.playerListText}>
+            <Text style={{ color: 'white', paddingRight: '2%', width: '35%' }} ellipsizeMode='tail'>{player.name}</Text>
+            <View style={{ width: '50%' }}>
+                <Button
+                    onPress={() => {
                         setPlayerDrinks(!playerDrinks);
-                        togglePlayerDrinks(player.id);
+                        togglePlayerDrinks(player);
                     }}
-                    title = {playerDrinks ? "trinkt" : "trinkt nicht"}
-                    color={ playerDrinks ? 'green' : 'red' }
+                    title={playerDrinks ? "trinkt" : "trinkt nicht"}
+                    color={playerDrinks ? 'green' : 'red'}
                 />
             </View>
-        )
+            <View style={{ paddingLeft: '2%' }}>
+                <Button
+                    onPress={() => {
+                        removePlayer(player);
+                    }}
+                    title={'-'}
+                    color={'red'}
+                />
+            </View>
+        </View>
+    )
 };
 
 export default NameContainer;
