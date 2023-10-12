a41b160c258d10c9a1e445eb824aeda7ac2ae6f5
Flowwrian <flo.haberkorn@gmx.de>
2023-10-12 15:27:21 +0200

    players are now global objects with multiple fields; changed AddPlayer menu

--- Trinkspiel_RN/src/menus/AddPlayer.js
+++ Trinkspiel_RN/src/menus/AddPlayer.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useCallback } from 'react';
 import { Button, View, Text, TextInput, StyleSheet, TouchableOpacity, FlatList, ImageBackground, ScrollView } from 'react-native';
 import { appStyles } from '../../styles';
 import { VariablesContext } from '../../VariablesContext';
+import NameContainer from './sublements/AddPlayerNameContainer';
 
 const PlayerInput = React.memo(({ onAddPlayer }) => {
   const [currentName, setCurrentName] = useState('');
@@ -14,7 +15,7 @@ const PlayerInput = React.memo(({ onAddPlayer }) => {
   };
 
   return (
-    <>
+    <View>
       <Text style={appStyles.title}>Enter Player Names</Text>
       <TextInput
         placeholder="Enter a name"
@@ -24,44 +25,50 @@ const PlayerInput = React.memo(({ onAddPlayer }) => {
         style={appStyles.input}
       />
       <TouchableOpacity onPress={handleAddPlayer} style={appStyles.chalkboardButton}>
-        <Text style={[appStyles.chalkboardButtonText, {fontSize:20}]}>Add Player</Text>
+        <Text style={[appStyles.chalkboardButtonText, { fontSize: 20 }]}>Add Player</Text>
       </TouchableOpacity>
-    </>
+    </View>
   );
 });
+
+//for tracking different player ids
+let player_id = 0;
 
 const AddPlayer = ({ navigation }) => {
   const { playerNames, setPlayerNames } = useContext(VariablesContext);
 
   const handleAddPlayer = useCallback(
     (name) => {
-      setPlayerNames([...playerNames, name]);
+      setPlayerNames([...playerNames, {id: player_id, name, drinks: true}]);
+      player_id += 1;
     },
     [playerNames]
   );
 
   return (
-    <ImageBackground source={require("../../assets/images/bar/settings_tafel.png")} style={{flex: 1}}>
+    <ImageBackground source={require("../../assets/images/bar/settings_tafel.png")} style={{ flex: 1 }}>
       <View style={appStyles.menuContainer}>
-        <View style={{width:'70%'}}>
+        <View style={{ width: '70%' }}>
           <PlayerInput onAddPlayer={handleAddPlayer} />
-          
-              <View style={{ height: 200 }}>
-                <FlatList
-                data={playerNames}
-                renderItem={({ item }) => <Text style={appStyles.playerListText}>{item}</Text>}
-                keyExtractor={(item, index) => index.toString()}
-                style={appStyles.playerList}
-              />
-              </View>
+
+          <View style={{ height: 200 }}>
+            <FlatList
+              data={playerNames}
+              renderItem={({ item }) =>
+                <NameContainer player={item} />
+              }
+              keyExtractor={(item, index) => index.toString()}
+              style={appStyles.playerList}
+            />
+          </View>
 
 
         </View>
         <TouchableOpacity onPress={() => navigation.navigate('PicoloGame')} style={[appStyles.chalkboardButton, {}]}>
           <Text style={appStyles.chalkboardButtonText}>Spielen</Text>
         </TouchableOpacity>
-      
-        </View>
+
+      </View>
 
     </ImageBackground>
 
