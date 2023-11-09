17fc86511943f7278e7dd6f149ee3402a4269287
Richard Bendler <richard.bendler@gmail.com>
2023-11-09 18:16:06 +0100

    sql abfragen wieder rein, funktioniert aber noch nicht

--- Trinkspiel_RN/src/general.js
+++ Trinkspiel_RN/src/general.js
@@ -7,8 +7,8 @@ export const handleSqlRequest = async (sqlRequest) => {
 const token = "Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFwcCJ9LCJpYXQiOjE2OTExNzU2OTV9.TqiVCGJdiq8lgn9-akwwzoRLxR5KZhllRXr_yWQL9JE"; // Token generieren und hier einfügen
 ret = '';
 try {
-    const response = await fetch('http://45.9.63.16:3000/api/sqlRequest', {
-    //const response = await fetch('https://my-tournament.org/api/sqlRequest', {
+    //const response = await fetch('http://45.9.63.16:3000/api/sqlRequest', {
+    const response = await fetch('https://my-tournament.org:3000/api/sqlRequest', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
