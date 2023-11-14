2416e2fb5276a025744663bddd5d9c7aa10e42c0
richardbendler <richard.bendler@gmail.com>
2023-11-14 22:50:34 +0100

    sync

--- Trinkspiel_RN/src/menus/StartMenu.js
+++ Trinkspiel_RN/src/menus/StartMenu.js
@@ -6,6 +6,57 @@ import SettingsButton from './sublements/SettingsButton';
 import Settings from './sublements/Settings';
 import NetInfo from "@react-native-community/netinfo";
 
+//HANDLE SQL REQUESTS
+const handleSqlRequest = async (sqlRequest) => {
+    const token = "Bearer "+"REDACTED_JWT"; // Token generieren und hier einfügen
+    ret = '';
+    try {
+        //const response = await fetch('http://45.9.63.16:3000/api/sqlRequest', {
+        const response = await fetch('https://my-tournament.org:8443/api/sqlRequest', {
+        //https nutzt Port 443
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token,
+        },
+        body: JSON.stringify({ sqlRequest }),
+        });
+    
+        if (response.ok) {
+        const responseText = await response.text();
+        ret = JSON.parse(responseText);
+        } else {
+            console.log(response)
+            console.error('Fehler beim Senden des Texts.');
+        }
+    } catch (error) {
+        console.error('Ein Fehler ist aufgetreten:', error);
+    }
+    //console.log(ret);
+    return ret;
+};
+
+const handleTestAPI = async () => {
+    try {
+        const response = await fetch('https://www.codeyourapp.de/tools/query.php?count=5&mode=0')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Fehler beim Senden des Texts.');
+                }
+                console.log(response);
+                return response.text();
+            })
+            .catch(error => {
+                console.error('Fehler bei der Anfrage:', error);
+                throw error; // Fehler weiterwerfen, um ihn in der nächsthöheren Funktion zu behandeln
+            });
+
+        return response; // Rückgabe der tatsächlichen Antwort
+    } catch (error) {
+        console.error('Ein Fehler ist aufgetreten:', error);
+        throw error; // Fehler weiterwerfen, um ihn in der nächsthöheren Funktion zu behandeln
+    }
+};
 
 const StartMenu = ({ navigation }) => {
     const { settingsVisible, setSettingsVisible } = useContext(VariablesContext);
@@ -23,6 +74,28 @@ const StartMenu = ({ navigation }) => {
           unsubscribe();
         };
       }, []);
+
+    //Zugriff Test-API
+    const [ret, setRet] = useState(["Platzhalter"]);
+    useEffect(() => {
+        const fetchData = async () => {
+        const result = await handleTestAPI();
+        setRet(JSON.stringify(result));
+    };
+    fetchData();
+    }, []);
+
+     //Activity
+    const [words, setWords] = useState(["Platzhalterfrage"]);
+    useEffect(() => {
+        const fetchData = async () => {
+        const result = await handleSqlRequest('SELECT * FROM `game_activity_words`');
+        setWords(JSON.stringify(result));
+    };
+    fetchData();
+    }, []);
+
+     
 
 
   return (
@@ -42,6 +115,10 @@ const StartMenu = ({ navigation }) => {
                     <TouchableOpacity onPress={() => navigation.navigate('MainMenu')} style={appStyles.chalkboardButton}>
                         <Text style={appStyles.chalkboardButtonText}>Spielen</Text>
                     </TouchableOpacity>
+
+                    <Text>{words[0].content}</Text>
+                    <Text>--------------------------</Text>
+                    <Text>{ret}</Text>
 
                     {/*<TouchableOpacity onPress={() => navigation.navigate('MainMenu')} style={appStyles.menuButton}>
                         <Text style={appStyles.menuButtonText}>Custom Game</Text>
