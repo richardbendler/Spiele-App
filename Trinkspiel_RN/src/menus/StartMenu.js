38ff8e02871d62381b3a7fb539aad576edb1e66d
Richard Bendler <richard.bendler@gmail.com>
2023-11-14 16:10:51 +0100

    Internet-Abfrage auf Startbildschirm

--- Trinkspiel_RN/src/menus/StartMenu.js
+++ Trinkspiel_RN/src/menus/StartMenu.js
@@ -1,12 +1,28 @@
-import React, { useState, useContext, useCallback } from 'react';
+import React, { useState, useContext, useCallback, useEffect } from 'react';
 import { Button, View, Text, TextInput, StyleSheet, TouchableOpacity, FlatList, Modal, ImageBackground } from 'react-native';
 import { appStyles } from '../../styles';
 import { VariablesContext } from '../../VariablesContext';
 import SettingsButton from './sublements/SettingsButton';
 import Settings from './sublements/Settings';
+import NetInfo from "@react-native-community/netinfo";
+
 
 const StartMenu = ({ navigation }) => {
     const { settingsVisible, setSettingsVisible } = useContext(VariablesContext);
+
+    const [isConnected, setIsConnected] = useState(true);
+    useEffect(() => {
+        // Überwache die Internetverbindung
+        const unsubscribe = NetInfo.addEventListener(state => {
+          setIsConnected(state.isConnected);
+        });
+    
+        return () => {
+          // Stelle sicher, dass du das Abonnement auflöst, um Speicherlecks zu vermeiden
+          unsubscribe();
+        };
+      }, []);
+
 
   return (
     <ImageBackground source={require("../../assets/images/bar/bar_image_complete.png")} style={{flex: 1}}>
@@ -17,7 +33,9 @@ const StartMenu = ({ navigation }) => {
                 <View style={appStyles.menuContainer}>
                     
                     
-                    
+                    <Text>
+                        {isConnected ? 'Du bist mit dem Internet verbunden.' : 'Du bist nicht mit dem Internet verbunden.'}
+                    </Text>
 
 
                     <TouchableOpacity onPress={() => navigation.navigate('MainMenu')} style={appStyles.chalkboardButton}>
