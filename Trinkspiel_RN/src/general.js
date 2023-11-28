7b2a5b02825c198374fba795195aa95da8ed9dd9
Richard Bendler <richard.bendler@gmail.com>
2023-11-28 10:59:58 +0100

    API läuft so aktuell nur im Entwicklermodus

--- Trinkspiel_RN/src/general.js
+++ Trinkspiel_RN/src/general.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, TouchableOpacity, Animated, StyleSheet } from 'react-native';
-/*import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const saveToStorage = async (item, value) => {
     try {
@@ -44,34 +44,4 @@ export const handleSqlRequestAndSafeToDisk = async (storageItem, setter, sqlRequ
         alert(error)
     }
     
-};*/
-
-export const handleSqlRequest = async (sqlRequest) => {
-    const token = "Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6ImFwcCJ9LCJpYXQiOjE2OTExNzU2OTV9.TqiVCGJdiq8lgn9-akwwzoRLxR5KZhllRXr_yWQL9JE"; // Token generieren und hier einfügen
-    ret = '';
-    try {
-        //const response = await fetch('http://45.9.63.16:3000/api/sqlRequest', {
-        const response = await fetch('https://my-tournament.org:8443/api/sqlRequest', {
-        //https nutzt Port 443
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token,
-        },
-        body: JSON.stringify({ sqlRequest }),
-        });
-    
-        if (response.ok) {
-        const responseText = await response.text();
-        ret = JSON.parse(responseText);
-        } else {
-            console.log(response)
-            console.error('Fehler beim Senden des Texts.');
-        }
-    } catch (error) {
-        console.error('Ein Fehler ist aufgetreten:', error);
-    }
-    //console.log(ret);
-    return ret;
-    };
-
+};
