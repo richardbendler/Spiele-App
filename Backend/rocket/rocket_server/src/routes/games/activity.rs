7738704c6a903c69eb2120a3bfd20e7645b3440b
richardbendler <richard.bendler@gmail.com>
2024-07-15 19:06:32 +0200

    activation und deletion von datenbankeinträgen in sql integriert - bugfix

--- Backend/rocket/rocket_server/src/routes/games/activity.rs
+++ Backend/rocket/rocket_server/src/routes/games/activity.rs
@@ -9,28 +9,28 @@ use crate::routes::games::common::*;
 
 // definition of data schema
 pub struct ActivityResultWithOptions {
-    id: i32,
-    word: Option<String>,
-    forbidden_words: Option<String>,
-    deletion: i32,
-    activation: i32,
-    category: Option<String>,
-    fk_difficulty: i32,
-    popularity: i32,
-    timestamp: OffsetDateTime
+    pub id: i32,
+    pub word: Option<String>,
+    pub forbidden_words: Option<String>,
+    pub deletion: i32,
+    pub activation: i32,
+    pub category: Option<String>,
+    pub fk_difficulty: i32,
+    pub popularity: i32,
+    pub timestamp: OffsetDateTime
 }
 
 #[derive(Serialize)]
 pub struct ActivityResult {
-    id: i32,
-    word: String,
-    forbidden_words: String,
-    deletion: i32,
-    activation: i32,
-    category: String,
-    fk_difficulty: i32,
-    popularity: i32,
-    timestamp: i64
+    pub id: i32,
+    pub word: String,
+    pub forbidden_words: String,
+    pub deletion: i32,
+    pub activation: i32,
+    pub category: String,
+    pub fk_difficulty: i32,
+    pub popularity: i32,
+    pub timestamp: i64
 }
 
 
@@ -42,7 +42,21 @@ pub async fn query(pool: &State<Pool<MySql>>, _key: AppKey<'_>) -> Json<Response
     let unwraped_pool = pool.inner();
     let results: Vec<ActivityResult> = sqlx::query_as!(
         ActivityResultWithOptions,
-        "SELECT * FROM `game_activity_words` WHERE deletion = 0 AND activation = 1"
+        "SELECT 
+            game_activity_words.id as id,
+            game_activity_words.word as word,
+            game_activity_words.forbidden_words as forbidden_words,
+            game_activity_words.deletion as deletion,
+            game_activity_words.activation as activation,
+            game_activity_words.category as category,
+            game_activity_words.fk_difficulty as fk_difficulty,
+            game_activity_words.popularity as popularity,
+            game_activity_words.timestamp as timestamp
+         FROM 
+            `game_activity_words` 
+        WHERE 
+            game_activity_words.deletion = 0 
+            AND game_activity_words.activation = 1"
     )
     .fetch_all(unwraped_pool)
     .await
